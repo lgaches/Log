bc686fadd632b142068eb0b674a80fdb0766d6bd
Laurent Gaches <laurent@binimo.com>
2018-01-11 17:55:22 +0100

    Reinventing the wheel. Again.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Log",
+    products: [
+        .library(
+            name: "Log",
+            targets: ["Log"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Log",
+            dependencies: []),
+        .testTarget(
+            name: "LogTests",
+            dependencies: ["Log"]),
+    ]
+)
---
+++ Sources/Log/Log.swift
@@ -0,0 +1,41 @@
+public enum LogLevel {
+    case verbose
+    case debug
+    case info
+    case warning
+    case error
+}
+
+public protocol Log {
+
+    func log(_ level: LogLevel, _ message: @autoclosure () -> Any, _ file: String, _ function: String, line: Int, context: Any?)
+}
+
+public extension Log {
+
+    /// log something generally unimportant (lowest priority)
+    public func verbose(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil)  {
+        log(.verbose, message, file, function, line: line, context: context)
+    }
+
+    /// log something which help during debugging (low priority)
+    public func debug(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+        log(.debug, message, file, function, line: line, context: context)
+    }
+
+    /// log something which you are really interested but which is not an issue or error (normal priority)
+    public func info(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+        log(.info, message, file, function, line: line, context: context)
+
+    }
+
+    /// log something which may cause big trouble soon (high priority)
+    public func warning(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+        log(.warning, message, file, function, line: line, context: context)
+    }
+
+    /// log something which will keep you awake at night (highest priority)
+    public func error(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+        log(.error, message, file, function, line: line, context: context)
+    }
+}
