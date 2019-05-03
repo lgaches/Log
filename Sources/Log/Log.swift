c6bd80df13c15877f44390526d129dd190eb3b5c
Laurent Gaches <laurent@binimo.com>
2019-05-03 13:03:34 +0200

    fixed warning

--- Sources/Log/Log.swift
+++ Sources/Log/Log.swift
@@ -14,28 +14,28 @@ public protocol Log {
 public extension Log {
 
     /// log something generally unimportant (lowest priority)
-    public func verbose(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil)  {
+    func verbose(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil)  {
         log(.verbose, message, file, function, line: line, context: context)
     }
 
     /// log something which help during debugging (low priority)
-    public func debug(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+    func debug(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
         log(.debug, message, file, function, line: line, context: context)
     }
 
     /// log something which you are really interested but which is not an issue or error (normal priority)
-    public func info(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+    func info(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
         log(.info, message, file, function, line: line, context: context)
 
     }
 
     /// log something which may cause big trouble soon (high priority)
-    public func warning(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+    func warning(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
         log(.warning, message, file, function, line: line, context: context)
     }
 
     /// log something which will keep you awake at night (highest priority)
-    public func error(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
+    func error(_ message: @autoclosure () -> Any, _ file: String = #file, _ function: String = #function, line: Int = #line, context: Any? = nil) {
         log(.error, message, file, function, line: line, context: context)
     }
 }
